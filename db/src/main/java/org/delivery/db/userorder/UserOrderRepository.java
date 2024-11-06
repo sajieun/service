5d6e83493dd6d33cc6e57e6a0ef9e517046ddf2d
sajieun <sajieun000721@naver.com>
2024-11-06 21:07:40 +0900

    userorder db create finsh

--- db/src/main/java/org/delivery/db/userorder/UserOrderRepository.java
+++ db/src/main/java/org/delivery/db/userorder/UserOrderRepository.java
@@ -1,4 +1,6 @@
 package org.delivery.db.userorder;
 
-public interface UserOrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserOrderRepository extends JpaRepository<UserOrderEntity,Long> {
 }

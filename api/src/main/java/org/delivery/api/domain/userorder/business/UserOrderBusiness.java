2cfbc01e8d8c5204f8aac753a2e3d54361c84f29
sajieun <sajieun000721@naver.com>
2024-11-27 16:13:20 +0900

    sse 통한 사용자 주문 ing

--- api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
+++ api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
@@ -12,6 +12,7 @@ import org.delivery.api.domain.userorder.controller.model.UserOrderDetailRespons
 import org.delivery.api.domain.userorder.controller.model.UserOrderRequest;
 import org.delivery.api.domain.userorder.controller.model.UserOrderResponse;
 import org.delivery.api.domain.userorder.converter.UserOrderConverter;
+import org.delivery.api.domain.userorder.producer.UserOrderProducer;
 import org.delivery.api.domain.userorder.service.UserOrderService;
 import org.delivery.api.domain.userordermenu.converter.UserOrderMenuConverter;
 import org.delivery.api.domain.userordermenu.service.UserOrderMenuService;
@@ -37,6 +38,8 @@ public class UserOrderBusiness {
 
     private final StoreService storeService;
     private final StoreConverter storeConverter;
+
+    private final UserOrderProducer userOrderProducer;
 
 
     // 1. 사용자 , 메뉴 id
@@ -68,6 +71,8 @@ public class UserOrderBusiness {
             userOrderMenuService.order(it);
         });
 
+        // 비동기로 가맹점에 주문한거 알리기
+        userOrderProducer.sendOrder(newUserOrderEntity);
 
         // response
         return userOrderConverter.toResponse(newUserOrderEntity);
---
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/consumer/UserOrderConsumer.java
@@ -0,0 +1,21 @@
+package org.delivery.storeadmin.domain.userorder.consumer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.delivery.common.message.model.UserOrderMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserOrderConsumer {
+
+    @RabbitListener(queues = "delivery.queue")
+    public void userOrderConsumer(
+            UserOrderMessage userOrderMessage
+    ){
+        log.info("message queue >> {}" , userOrderMessage);
+        System.out.println(userOrderMessage);
+    }
+}

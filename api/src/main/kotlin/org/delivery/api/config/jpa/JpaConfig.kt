dd86a3cbfe7aa7cf931a90b8fcdb63c35c7e6f77
sajieun <sajieun000721@naver.com>
2024-12-11 16:20:20 +0900

    java jpa -> kotlin move

---
+++ api/src/main/kotlin/org/delivery/api/config/jpa/JpaConfig.kt
@@ -0,0 +1,11 @@
+package org.delivery.api.config.jpa
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EntityScan(basePackages = ["org.delivery.db"]) // 이렇게 해야지 api가 db를 사용할 때 오류가 없어진다 !
+@EnableJpaRepositories(basePackages = ["org.delivery.db"])
+class JpaConfig {
+}
